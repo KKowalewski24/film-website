d2eb12d578186b7099516f0caec8092826d5ff72
Kamil Kowalewski <szamil128@onet.pl>
2020-02-11 12:30:50 +0100

    Add config + util files from previous repo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-import './index.css';
-import App from './main/App';
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import "./index.css";
+import App from "./main/App";
+import ContextProvider from "./main/controller/ContextProvider";
+
+ReactDOM.render(
+  <ContextProvider>
+    <App/>
+  </ContextProvider>,
+  document.getElementById("root"));
 
 serviceWorker.unregister();
